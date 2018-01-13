c61892ae083422f828ffaf4981a311f610830bba
Rob Lim <robert.m.lim@gmail.com>
2018-01-12 23:05:19 -0700

    Create graphql user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,7 @@
-Types::UserType = GraphQL::ObjectType.define do 
+Types::UserType = GraphQL::ObjectType.define do
+  name "User"
 
+  field :id, !types.ID
+  field :name, !types.String
+  field :email, !types.String
 end
-
